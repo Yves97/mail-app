4a39a2056a58265d4f5ca917a2aa5c953c944b2f
yvnan2.19 <yveskouadio111@yahoo.fr>
2020-02-07 13:37:18 +0000

    ending sidebar component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <div>
     <div class="container">
-      
+      <div class="mail-box">
+        <app-sidebar></app-sidebar>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-
+  import Sidebar from './components/Sibebar.vue'
 
 export default {
-  
+  components: {
+    appSidebar: Sidebar
+  }
 }
 </script>
--- src/components/Sibebar.vue
+++ src/components/Sibebar.vue
@@ -1,5 +1,35 @@
 <template>
-    
+    <aside class="sm-side">
+        <div class="user-head">
+            <img src="../assets/images/profile.jpg">
+            <div class="user-name">
+                <h5>Bo Anderson</h5>
+                <span class="email-address">yveskouadio111@yahoo.fr</span>
+            </div>
+        </div>
+        <ul class="inbox-nav">
+            <li class="active">
+                <a href="#">
+                    <i class="fa fa-inbox" aria-hidden="true"></i>Inbox <span class="label label-danger pull-right">?</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-envelope-o" aria-hidden="true"></i>Sent <span class="label label-default pull-right">?</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-bookmark-o" aria-hidden="true"></i>Important<span class="label label-warning pull-right">?</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-trash-o" aria-hidden="true"></i>Trash <span class="label label-default pull-right">?</span>
+                </a>
+            </li>
+        </ul>
+    </aside>
 </template>
 
 <script>
